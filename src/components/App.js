1a563a40b8344afe30f3e1e4f6927a0dd6cb411b
neyser zana <neyserj.zana@gmail.com>
2019-02-16 22:26:29 -0500

    creating landing page for users to input the city they want to see the forecast for

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
+import CityModel from './CityModel';
 
 class App extends Component {
   constructor() {
     super(),
-    this.state = {
+      this.state = {
+        url: 'https://api.openweathermap.org/data/2.5/forecast?appid=fe350fdd5cf2042a016c2eca2ef6252c&q=',
+        city: '',
+        forecast: {},
+      }
+  }
 
-    }
+  handleUserSearch(e) {
+    const city = this.state.city.trim();
+    const url = `${this.state.url}${city}`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(err => new Error(err));
+  }
+
+  handleUserInput(e) {
+    const userInput = e.target.value;
+
+    this.setState({city: userInput});
   }
 
   render() {
     return (
-      <h1 id='title'>Weather Forecast</h1>
+      <div id='main-body'>
+        <h1 id='main-body-title'>Weather Forecast</h1>
+        <CityModel
+          handleSearch={this.handleUserSearch.bind(this)}
+          userInput={this.state.city}
+          handleInput={this.handleUserInput.bind(this)}
+        />
+      </div>
     );
   }
 }
--- src/components/CityModel.js
+++ src/components/CityModel.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+const CityModel = (props) => {
+  return (
+    <div id='city-model'>
+      <h2 id='city-model-title'>Search for a location</h2>
+      <input type="text" id='city-model-input' placeholder='Enter a city' value={props.userInput} onChange={(e) => { props.handleInput(e) }} />
+      <button id='city-model-btn' onClick={(e) => { props.handleSearch(e) }}>Search</button>
+    </div>
+  );
+}
+
+export default CityModel;
