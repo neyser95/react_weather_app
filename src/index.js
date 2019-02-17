a5f7cdb694130178a00974f319d9b9f195195603
neyser zana <neyserj.zana@gmail.com>
2019-02-16 22:26:58 -0500

    setting up correct scss paths

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './components/App.js';
 
-import styles from './scss/app.scss';
+import styles from './scss/main.scss';
 
 render(<App/>, document.getElementById('root'));
 
 
-module.hot.accept(['./components/App.js', './scss/app.scss'], () => {
+module.hot.accept(['./components/App.js', './scss/main.scss'], () => {
   const NextApp = require('./components/App.js').default;
   render(
     <NextApp />, document.getElementById('root')
